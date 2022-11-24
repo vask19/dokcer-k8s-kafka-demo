4b10856f4a89b60df413d6ba78f196f4f2943234
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-11-24 21:52:59 +0100

    micr

---
+++ micros/spring-admin/src/main/java/com/example/springadmin/SpringAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+
+}
