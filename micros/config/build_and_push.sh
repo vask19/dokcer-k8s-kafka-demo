98b7f009949641186be5b5d6007b432092c5a6c7
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-11-25 10:18:02 +0100

    micros

--- micros/config/build_and_push.sh
+++ micros/config/build_and_push.sh
@@ -1,6 +1,14 @@
+cp config/Dockerfile $1/Dockerfile
+cd $1
+
 ./gradlew clean build
-docker build . -t vask191/cats-api:1.0.0
-docker push vask191/cats-api:1.0.0
+docker build . -t vask191/$1:$2
+docker push vask191/$1:$2
+
+
+rm Dockerfile
+
+
 #chmod +x build_and_push.sh
 #docker run -ti --rm -e DATASOURCE_HOST=172.20.10.2 -p 8081:8080 vask191/cats-api:1.0.0
 #ifconfig
