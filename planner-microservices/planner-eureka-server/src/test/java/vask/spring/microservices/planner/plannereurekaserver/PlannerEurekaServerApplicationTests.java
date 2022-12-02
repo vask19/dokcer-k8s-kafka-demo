47186d61d0b572f54d5ea34824d5691166206963
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-12-02 22:03:49 +0100

    created new project

---
+++ planner-microservices/planner-eureka-server/src/test/java/vask/spring/microservices/planner/plannereurekaserver/PlannerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vask.spring.microservices.planner.plannereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ planner-microservices/src/main/java/vask/spring/microservices/planner/plannermicroservices/PlannerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package vask.spring.microservices.planner.plannermicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerMicroservicesApplication.class, args);
+    }
+
+}
