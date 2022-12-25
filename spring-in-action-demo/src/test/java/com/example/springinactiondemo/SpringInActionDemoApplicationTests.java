5190785c4fd694e59e81d9bfd1c1b3d050cfb711
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-12-25 19:55:04 +0100

    createn project for book sia

---
+++ spring-in-action-demo/src/test/java/com/example/springinactiondemo/SpringInActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
