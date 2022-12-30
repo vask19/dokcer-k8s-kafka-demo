6693938768602db58848faca31e8d91e7342503f
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-12-30 11:05:38 +0100

    config-service has been created and configurated

---
+++ Ostock/config-serve/src/main/java/vask/ostock/configserve/ConfigServeApplication.java
@@ -0,0 +1,15 @@
+package vask.ostock.configserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServeApplication.class, args);
+    }
+
+}
---
+++ Ostock/config-serve/src/test/java/vask/ostock/configserve/ConfigServeApplicationTests.java
@@ -0,0 +1,13 @@
+package vask.ostock.configserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
