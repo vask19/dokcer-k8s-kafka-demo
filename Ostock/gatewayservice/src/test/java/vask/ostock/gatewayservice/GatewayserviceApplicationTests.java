f36102a8c6b9972fed6fb38ed9a88d9666ea2c1e
Vasyl Korol mac <gusak.guska223@gmail.com>
2023-01-09 18:15:30 +0100

    spring gateway api has been configured

---
+++ Ostock/gatewayservice/src/test/java/vask/ostock/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package vask.ostock.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
