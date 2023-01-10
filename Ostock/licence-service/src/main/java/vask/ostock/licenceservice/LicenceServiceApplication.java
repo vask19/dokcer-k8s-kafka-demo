00f3411a2f5c7e885032e9b02ea5b5806e61a9bf
Vasyl Korol mac <gusak.guska223@gmail.com>
2023-01-10 10:30:58 +0100

    keyckloak has been configured

--- Ostock/licence-service/src/main/java/vask/ostock/licenceservice/LicenceServiceApplication.java
+++ Ostock/licence-service/src/main/java/vask/ostock/licenceservice/LicenceServiceApplication.java
@@ -10,14 +10,18 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+import vask.ostock.licenceservice.util.UserContextInterceptor;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @RefreshScope
 @EnableFeignClients
 @ConfigurationPropertiesScan
 @EnableDiscoveryClient
-
 public class LicenceServiceApplication {
 
     public static void main(String[] args) {
@@ -27,7 +31,16 @@ public class LicenceServiceApplication {
 
     @LoadBalanced
     @Bean
-    public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+    public RestTemplate getRestTemplate() {
+        RestTemplate template = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+        if (interceptors.isEmpty()) {
+            template.setInterceptors(Collections.singletonList(
+                    new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+        return template;
     }
 }
